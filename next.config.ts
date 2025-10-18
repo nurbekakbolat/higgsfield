eba442df17a96da7252d9c062bfa0e959ab37829
nurbekakbolat <nurbekakbolat9@gmail.com>
2025-10-19 01:20:08 +0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- experimental: {
+  experimental: {
     serverActions: {
-      bodySizeLimit: '50mb',
+      bodySizeLimit: "50mb",
     },
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false; // ignore node-canvas
+    config.resolve.alias["pdfjs-dist/build/pdf"] =
+      "pdfjs-dist/legacy/build/pdf";
+    config.resolve.alias["pdfjs-dist/build/pdf.worker"] =
+      "pdfjs-dist/legacy/build/pdf.worker";
+    return config;
   },
 };
 
